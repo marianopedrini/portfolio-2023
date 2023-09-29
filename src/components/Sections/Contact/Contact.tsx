4ebdaedf04e0b17dd75c67384ed1f2f9d489a772
Mariano <pedrini.mariano96@gmail.com>
2023-09-29 00:46:25 -0300

    add contact animation (NOT FINISHED)

--- src/components/Sections/Contact/Contact.tsx
+++ src/components/Sections/Contact/Contact.tsx
@@ -1,13 +1,28 @@
+import { useLayoutEffect, useRef } from "react";
 import Link from "next/link";
+import { gsap } from "gsap";
 
 import Section from "@/components/Section/Section"
 import Title from "@/components/Title/Title"
+
 import { contactLinks } from "@/data";
+import { animateContact } from "./animations";
 
 const Contact = () => {
+    const contactSectionRef = useRef<HTMLElement>(null)
+    const contactCardRef = useRef<HTMLDivElement>(null)
+
+    useLayoutEffect(() => {
+      const ctx = gsap.context(() => {
+        animateContact(contactSectionRef, contactCardRef)
+      })
+    
+      return () => ctx.revert()
+    }, [])
+
   return (
-    <Section extraClasses="h-[100dvh] md:pb-0" id="contact">
-      <div className="container bg-white flex flex-col pt-20 rounded-t-2xl h-full">
+    <Section extraClasses="h-[100dvh] md:pb-0" id="contact" forwardedRef={contactSectionRef}>
+      <div className="container bg-white flex flex-col pt-20 rounded-t-2xl h-full" ref={contactCardRef}>
         <Title
           title={'Contact'}
           color={'black'}
---
+++ src/components/Sections/Contact/animations.tsx
@@ -0,0 +1,14 @@
+import { gsap } from 'gsap';
+
+export const animateContact = (contactSectionRef: any, contactCardRef: any) => {
+    // gsap.to(contactCardRef.current, {
+    //     yPercent: -100,
+    //     scrollTrigger: {
+    //         trigger: contactSectionRef.current,
+    //         start: 'top bottom',
+    //         end: 'end bottom',
+    //         markers: true,
+    //         scrub: true,
+    //     }
+    // })
+}
