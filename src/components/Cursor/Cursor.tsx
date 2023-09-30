306bbdda0ee1eddd693d361e940cc26cd704cebc
Mariano <pedrini.mariano96@gmail.com>
2023-09-30 19:19:36 -0300

    fix custom cursor

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -1,6 +1,5 @@
 import { useLayoutEffect, useRef, useState } from "react"
 import gsap from "gsap"
-import { useGsapContext } from "@/hooks/useGsapContext"
 
 const Cursor = () => {
     const [hoveringLink, setHoveringLink] = useState(false)
@@ -36,7 +35,7 @@ const Cursor = () => {
 
     
   return (
-    <div className="fixed top-0 left-0 w-5 h-5 rounded-full pointer-events-none z-[100] mix-blend-difference p-2.5 flex justify-center items-center bg-white" id="custom-cursor" ref={cursorRef}>
+    <div className="fixed top-0 left-0 w-5 h-5 rounded-full pointer-events-none z-[200] mix-blend-difference p-2.5 flex justify-center items-center bg-white" id="custom-cursor" ref={cursorRef}>
         <p className="text-2xl font-semibold hidden" id="cursor-text"> Cursor</p>
     </div>
   )
