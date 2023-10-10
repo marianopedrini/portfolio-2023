cdf0dec324d9205bc83deca7ff0d363669baf80d
marianopedrini <pedrini.mariano96@gmail.com>
2023-10-10 14:06:39 -0300

    fix styles in About and globals

--- src/components/Sections/About/About.tsx
+++ src/components/Sections/About/About.tsx
@@ -29,7 +29,7 @@ const About = () => {
           ref={containerRef}
         >
           {aboutTexts.map((text, index) => (
-            <p className="opacity-0 text-3xl" key={index} data-about-text>
+            <p className="opacity-0 text-2xl md:text-3xl" key={index} data-about-text>
               {text}
             </p>
           ))}
