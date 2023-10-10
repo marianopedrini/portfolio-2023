87b6df339bf8d5d17937599aa343c34a89217191
marianopedrini <pedrini.mariano96@gmail.com>
2023-10-10 11:30:45 -0300

    add contact animations

--- src/components/Sections/Contact/Contact.tsx
+++ src/components/Sections/Contact/Contact.tsx
@@ -1,41 +1,60 @@
-import { useLayoutEffect, useRef } from "react";
-import Link from "next/link";
-import { gsap } from "gsap";
+import { useLayoutEffect, useRef } from 'react';
+import Link from 'next/link';
+import { gsap } from 'gsap';
 
-import Title from "@/components/Title/Title"
+import Title from '@/components/Title/Title';
 
-import { contactLinks } from "@/data";
-import { animateContact } from "./animations";
+import { contactLinks } from '@/data';
+import { animateContact } from './animations';
 
 const Contact = () => {
-    const contactSectionRef = useRef<HTMLElement>(null)
-    const contactCardRef = useRef<HTMLDivElement>(null)
+  const contactSectionRef = useRef<HTMLElement>(null);
+  const titleRef = useRef<HTMLHeadingElement>(null);
 
-    useLayoutEffect(() => {
-      const ctx = gsap.context(() => {
-        animateContact(contactSectionRef, contactCardRef)
-      })
-    
-      return () => ctx.revert()
-    }, [])
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      animateContact(contactSectionRef, titleRef);
+    }, [contactSectionRef]);
+
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <section className="relative z-30 h-[99dvh] md:h-[90dvh] pt-20 md:pt-28" id="contact" ref={contactSectionRef}>
-      <div className="container bg-white flex flex-col pt-20 rounded-t-2xl h-full" ref={contactCardRef}>
-        <Title
-          title={'Contact'}
-          color={'black'}
-          extraClasses="text-center md:col-start-1"
-        />
-        <h4 className="col-span-full text-center text-black text-2xl md:text-4xl font-medium">
-          Have an idea? Get in touch!
+    <section
+      className="relative z-30 h-[99dvh] md:h-[90dvh] pt-20 md:pt-28"
+      id="contact"
+      ref={contactSectionRef}
+    >
+      <div className="container bg-white flex flex-col pt-20 rounded-t-2xl h-full">
+        <div className="w-fit mx-auto overflow-hidden">
+          <Title
+            title={'Contact'}
+            color={'black'}
+            extraClasses="text-center md:col-start-1 translate-y-full opacity-0"
+            forwardedRef={titleRef}
+          />
+        </div>
+        <h4 className="w-fit mx-auto text-black font-medium overflow-hidden text-2xl md:text-4xl">
+          <span className="translate-y-full inline-block" data-contact-subtitle>
+            Have an idea?
+          </span>{' '}
+          <span
+            className="translate-y-full inline-block "
+            data-contact-subtitle
+          >
+            Get in touch!
+          </span>
         </h4>
 
         <div className="mt-20 md:mt-28">
           <nav>
             <ul className="flex flex-col gap-8 md:flex-row md:justify-evenly contact-links">
               {contactLinks.map((navlink) => (
-                <li className="text-center text-xl" key={navlink.link}>
+                <li
+                  className="opacity-0 -translate-y-3/4 text-center text-xl"
+                  key={navlink.link}
+                  data-contact-link
+                >
                   <Link
                     href={navlink.link}
                     target="_blank"
@@ -49,10 +68,12 @@ const Contact = () => {
             </ul>
           </nav>
         </div>
-        <h6 className="text-black font-semibold text-center mt-auto mb-6">Made by Mariano Pedrini</h6>
+        <h6 className="text-black font-semibold text-center mt-auto mb-6">
+          Made by Mariano Pedrini
+        </h6>
       </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
--- src/components/Sections/Contact/animations.tsx
+++ src/components/Sections/Contact/animations.tsx
@@ -1,14 +1,33 @@
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-export const animateContact = (contactSectionRef: any, contactCardRef: any) => {
-    // gsap.to(contactCardRef.current, {
-    //     yPercent: -100,
-    //     scrollTrigger: {
-    //         trigger: contactSectionRef.current,
-    //         start: 'top bottom',
-    //         end: 'end bottom',
-    //         markers: true,
-    //         scrub: true,
-    //     }
-    // })
-}
+gsap.registerPlugin(ScrollTrigger);
+
+export const animateContact = (contactSectionRef: any, titleRef: any) => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: contactSectionRef.current,
+      start: 'top center',
+      markers: true,
+    },
+  });
+  const subtitles = gsap.utils.toArray('[data-contact-subtitle]');
+  const links = gsap.utils.toArray('[data-contact-link]');
+
+  tl.to(titleRef.current, {
+    y: 0,
+    opacity: 1,
+  });
+  tl.to(subtitles, {
+    y: 0,
+    opacity: 1,
+    stagger: 0.2,
+  });
+  tl.to(links, {
+    y: 0,
+    opacity: 1,
+    stagger: 0.1,
+  });
+
+  return tl;
+};
